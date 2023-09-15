c6f58d1b3bee1a8e748669009df1eded7b3f5238
vyacheslavsafin <safin.vr@mail.ru>
2023-09-15 23:07:44 +0300

    настроено отображение в административной панели, сформирована фикстура для заполнения БД

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from catalog.models import Product
-
-admin.site.register(Product)
+from catalog.models import Product, Category
 
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name', 'description',)
+
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,7 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
 
     def __str__(self):
         return f'{self.name} ({self.description})'
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=250, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
     image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Изображение')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за покупку')

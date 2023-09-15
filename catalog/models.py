394ecb9387ab6c065f2a43efc128b4b735ce5ee1
vyacheslavsafin <safin.vr@mail.ru>
2023-09-15 18:17:03 +0300

    откат миграции

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,6 @@ NULLABLE = {'blank': True, 'null': True}
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
-    created_at = models.DateTimeField(**NULLABLE, verbose_name='Дата создания')
 
     def __str__(self):
         return f'{self.name} ({self.description})'

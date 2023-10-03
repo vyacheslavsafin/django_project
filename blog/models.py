555490d3f022abe3f34d61df030227dcfea1b8ac
vyacheslavsafin <safin.vr@mail.ru>
2023-10-03 23:57:29 +0300

    Создана новое приложение Blog с новой моделью Post, реализованы счетчик просмотров, вывод только активных записей, формирование slug для заголовков, перенаправление на статью после успешного редактирования

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug')
+    content = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='blog/', **NULLABLE, verbose_name='Превью')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    publication_sign = models.BooleanField(default=True, verbose_name='Публикация')
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи '
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
+from pytils.translit import slugify
 
-# Create your views here.
+from blog.models import Post
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    fields = ('title', 'content', 'preview')
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'content')
+    # success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
+
+class BlogListView(ListView):
+    model = Post
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(publication_sign=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:list')

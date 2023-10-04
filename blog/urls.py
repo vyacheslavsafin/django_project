797b9c02cbfd9127d632ed087a8a39a0474c95b4
vyacheslavsafin <safin.vr@mail.ru>
2023-10-04 12:49:45 +0300

    переделан идентификатор url на slug вместо pk

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', BlogCreateView.as_view(), name='create'),
     path('', BlogListView.as_view(), name='list'),
-    path('view/<int:pk>/', BlogDetailView.as_view(), name='view'),
-    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
-    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('view/<slug>/', BlogDetailView.as_view(), name='view'),
+    path('edit/<slug>/', BlogUpdateView.as_view(), name='edit'),
+    path('delete/<slug>/', BlogDeleteView.as_view(), name='delete'),
 ]
